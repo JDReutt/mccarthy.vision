d77c90113ae0f3a942fb2340b2e0215854ef90e6
Jdreutt <joe.reutt@gmail.com>
2017-11-28 01:18:19 -0500

    build for jdreutt

---
+++ docs/path---tags-portfolio-ef90fa02041f411b8e9a.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x35d944cf3233fa00],{"./node_modules/json-loader/index.js!./.cache/json/tags-portfolio.json":function(o,a){o.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/salon-ziba-black-white"},excerpt:"",timeToRead:1,frontmatter:{title:"Salon Ziba: Black & White",tags:["Portfolio","Commercial","Salon Ziba"],cover:"https://i.vimeocdn.com/video/663047185_1280x720.jpg?r=pad",date:"17/10/2017"}}}]}},pathContext:{tag:"Portfolio"}}}});
+//# sourceMappingURL=path---tags-portfolio-ef90fa02041f411b8e9a.js.map
